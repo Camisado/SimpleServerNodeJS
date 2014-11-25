85416995819232dc6aced6376be1ffb76c8fd248
Camisado <YG-92@yandex.ru>
2014-11-25 13:17:52 +0300

    heroku databased

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,10 @@
 var querystring = require("querystring");
 var MongoClient = require('mongodb').MongoClient
     , assert = require('assert');
-var url = 'mongodb://localhost:27017/mydb';
+var url = 'mongodb://camisado:test@ds053090.mongolab.com:53090/heroku_app31942135';
 
 function start(response, postData) {
     console.log("Request handler 'start' was called.");
-    var test = "qwe";
     MongoClient.connect(url, function(err, db) {
         assert.equal(null, err);
         console.log("Connected correctly to server");
@@ -14,7 +13,7 @@ function start(response, postData) {
         //collection.insert(doc1, function(err, result) {});
         collection.find({}, {"_id": 0}).toArray(function(err, docs) {
             console.log("Found the following records");
-            test = JSON.stringify(docs);
+            var test = JSON.stringify(docs);
             console.log(test);
             db.close();
             response.writeHead(200, {"Content-Type": "application/json"});
@@ -22,30 +21,28 @@ function start(response, postData) {
             response.end();
         });
     });
-
-//    var body = '<html>'+
-//        '<head>'+
-//        '<meta http-equiv="Content-Type" content="text/html; '+
-//        'charset=UTF-8" />'+
-//        '</head>'+
-//        '<body>'+
-//        '<form action="/upload" method="post">'+
-//        '<textarea name="text" rows="20" cols="60"></textarea>'+
-//        '<input type="submit" value="Submit text" />'+
-//        '</form>'+
-//        '</body>'+
-//        '</html>';
-
-
 }
 
-function upload(response, postData) {
-    console.log("Request handler 'upload' was called.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
+function add(response, postData) {
+    console.log("Request handler 'add' was called.");
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+        var collection = db.collection('test');
+        var doc1 = {'hello':'doc1'};
+        collection.insert(doc1, function(err, result) {
+            console.log("Inserted 1 document into the document collection");
+            db.close();
+            response.writeHead(200, {"Content-Type": "application/json"});
+            response.write(JSON.stringify(doc1));
+            response.end();
+        });
+    });
+    /*response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("You've sent the text: \n"+
     querystring.parse(postData).text);
-    response.end();
+    response.end();*/
 }
 
 exports.start = start;
-exports.upload = upload;
+exports.add = add;
